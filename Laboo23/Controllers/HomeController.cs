e58460ac3ceb716a1029f9e35edb07bc5589e13a
abiec013 <aec1020121@yahoo.com>
2017-10-26 15:08:31 -0400

    First Commit

---
+++ Laboo23/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Laboo23.Models;
+
+namespace Laboo23.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+
+            GroceriesEntities ORM = new GroceriesEntities();
+            List<Item> listofItems = ORM.Items.ToList(); //select * from customers
+            ViewBag.Message = "Your application description page.";
+            ViewBag.Clist = listofItems;
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult NewItem(Item Newitems)
+        {
+            GroceriesEntities GroceriesDB = new GroceriesEntities(); //este metodo es para mostrar en el view que se llama new employye para que display los datos del new employee
+            GroceriesDB.Items.Add(Newitems);// saves the new employee to the list 
+
+            GroceriesDB.SaveChanges(); //saves the new employee to the db
+
+            return RedirectToAction("About");
+        }
+
+        public ActionResult AddingNewItem()
+        {
+            return View();
+        }
+    }
+}
---
+++ Laboo23/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Laboo23.MvcApplication" Language="C#" %>
